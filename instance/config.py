4062005dbed8f9f414f1e74a40dc055bba96ea42
bien <hbien34@gmail.com>
2018-04-18 19:15:35 +0700

    update cloud sql

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,7 @@ MQTT_BROKER_PORT = 13894
 MQTT_USERNAME = 'vgomwcel'
 MQTT_PASSWORD = 'u8tzlVTr_da2'
 # SQL uri
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:1234@localhost:3306/mfarm?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'postgres://njqjsfyh:Z8F2-4wwa6U76J5eb6m4Uzz-IwsIjO__@elmer.db.elephantsql.com:5432/njqjsfyh'
 # Influxdb
 INFLUXDB_HOST = 'influxdb'
 INFLUXDB_PORT = 8086
